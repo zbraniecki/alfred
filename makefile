82306d0089194960fbd1f973b8b65d9a121f4673
Staś Małolepszy <stas@mozilla.com>
2016-09-09 21:43:30 +0000

    remove the bot part

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ export SHELL := /bin/bash
 export PATH  := $(CURDIR)/node_modules/.bin:$(PATH)
 
 MONGO_URL = 'mongodb://localhost:27017/alfred'
-PORT ?= 4001
+PORT ?= 4002
 
 build:
 	babel --presets es2015-loose --out-dir dist src
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import { MongoClient } from 'mongodb';
-import { createBot } from './bot';
 import { createServer } from './server';
 
 MongoClient
   .connect(process.env.MONGO_URL)
-  .then(start)
+  .then(createServer)
   .catch(console.error);
-
-function start(db) {
-  createBot(db);
-  createServer(db);
-}
