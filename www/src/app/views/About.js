325706f1cc96fdf83e683c59cbb7b0b60b245741
Staś Małolepszy <stas@mozilla.com>
2016-09-18 13:32:21 +0200

    align About's content

--- www/src/app/views/About.js
+++ www/src/app/views/About.js
@@ -12,35 +12,42 @@ export default function About() {
 
       <div className="content">
         <div className="content__tile">
-          <p>
-            Alfred is an IRC bot and a status board. It automatically tracks 
-            GitHub issues and pull requests, and listens to IRC conversations. 
-          </p>
-
-          <h3 className="report__subtitle">Open-source</h3>
-          <p>
-            Alfred is open-source.  You can view and contribute to the code 
-            at <a href="https://github.com/stasm/alfred">stasm/alfred</a>.  
-            Consult the current roadmap and report bugs on 
-            the <a href="https://github.com/stasm/alfred/projects">project board</a>.  
-            For a more real-time interaction join 
-            the <a href="irc://irc.mozilla.org/alfred">#alfred</a> channel on 
-            irc.mozilla.org.
-          </p>
-
-          <h3 className="report__subtitle">Webhook</h3>
-          <p>
-            Making Alfred know about the issues and pull request acticity in 
-            your repo is easy.  On GitHub, go to <em>Settings</em> 
-            → <em>Webhooks</em> and create a new webhook using the following 
-            <em>Payload URL</em>:
-          </p>
-          
-          <blockquote>
+          <section className="tile__section">
             <p>
-              <code>{WEBHOOK_URL}/github</code>
+              Alfred is an IRC bot and a status board. It automatically tracks 
+              GitHub issues and pull requests, and listens to IRC conversations. 
             </p>
-          </blockquote>
+
+            <h3 className="tile__subtitle">Open-source</h3>
+            <p>
+              Alfred is open-source.  You can view and contribute to the code 
+              at <a href="https://github.com/stasm/alfred">stasm/alfred</a>.  
+              Consult the current roadmap and report bugs on 
+              the <a href="https://github.com/stasm/alfred/projects">project board</a>.  
+              For a more real-time interaction join 
+              the <a href="irc://irc.mozilla.org/alfred">#alfred</a> channel on 
+              irc.mozilla.org.
+            </p>
+
+            <h3 className="tile__subtitle">Webhook</h3>
+            <p>
+              Making Alfred know about the issues and pull request acticity in 
+              your repo is easy.  On GitHub, go to <em>Settings</em> 
+              → <em>Webhooks</em> and create a new webhook using the following 
+              <em>Payload URL</em>:
+            </p>
+
+            <blockquote>
+              <p>
+                <code>{WEBHOOK_URL}/github</code>
+              </p>
+            </blockquote>
+
+            <p>
+              Currently Alfred supports the <code>issues</code> and 
+              the <code>pull_request</code> events.
+            </p>
+          </section>
         </div>
       </div>
 
