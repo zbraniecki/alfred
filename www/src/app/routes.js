5af9d791f502fc95d4f4e3f0408060b41bc05d56
Staś Małolepszy <stas@mozilla.com>
2016-09-26 13:27:59 +0200

    redirect from /:author to inbox

--- www/src/app/routes.js
+++ www/src/app/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, Redirect, browserHistory } from 'react-router';
 import App from './views/App';
 import About from './views/About';
 import ReportContainer from './views/ReportContainer';
@@ -13,6 +13,9 @@ export default (
     <Route path="report/:year-:month-:day" component={ReportContainer} />
     <Route path="review/:author/:year-:month-:day" component={ReviewContainer} />
     <Route path="inbox/:author" component={InboxContainer} />
+
+    // XXX remove this legacy url
+    <Redirect from=":author" to="inbox/:author" />
     </Route>
   </Router>
 )
