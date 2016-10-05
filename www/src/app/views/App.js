f482520bff4f6c0aa375a4fa3e8559442bf33128
Staś Małolepszy <stas@mozilla.com>
2016-10-05 17:03:42 +0200

    move global css to src/index.css

--- www/src/app/views/App.js
+++ www/src/app/views/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import './App.css';
-
 export default function App(props) {
   return (
     <div>
--- www/src/index.js
+++ www/src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 
 import routes from './app/routes';
 
+import './index.css';
+
 ReactDOM.render(
   routes,
   document.getElementById('root')
